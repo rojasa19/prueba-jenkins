c01ab574f5aa0fbd28119a359c5556196dfc6391
rojasa19 <rojasadrian.e@gmail.com>
2021-10-16 13:05:30 -0300

    correccion pruebas

--- app/converter.js
+++ app/converter.js
@@ -1,23 +1,28 @@
-exports.rgbToHex = function(red, green, blue) {
+var expect    = require("chai").expect;
+var converter = require("../app/converter");
 
-  var redHex   = red.toString(16);
-  var greenHex = green.toString(16);
-  var blueHex  = blue.toString(16);
+describe("Color Code Converter", function() {
+  describe("RGB to Hex conversion", function() {
+    it("converts the basic colors", function() {
+      var redHex   = converter.rgbToHex(255, 0, 0);
+      var greenHex = converter.rgbToHex(0, 255, 0);
+      var blueHex  = converter.rgbToHex(0, 0, 255);
 
-  return pad(redHex) + pad(greenHex) + pad(blueHex);
+      expect(redHex).to.equal("ff0000");
+      expect(greenHex).to.equal("00ff00");
+      expect(blueHex).to.equal("0000ff");
+    });
+  });
 
-};
+  describe("Hex to RGB conversion", function() {
+    it("converts the basic colors", function() {
+      var red   = converter.hexToRgb("ff0000");
+      var green = converter.hexToRgb("00ff00");
+      var blue  = converter.hexToRgb("0000ff");
 
-function pad(hex) {
-  return (hex.length === 1 ? "0" + hex : hex);
-}
-
-exports.hexToRgb = function(hex) {
-
-  var red   = parseInt(hex.substring(0, 2), 16);
-  var green = parseInt(hex.substring(2, 4), 16);
-  var blue  = parseInt(hex.substring(4, 6), 16);
-
-  return [red, green, blue];
-
-};
+      expect(red).to.deep.equal([255, 0, 0]);
+      expect(green).to.deep.equal([0, 255, 0]);
+      expect(blue).to.deep.equal([0, 0, 255]);
+    });
+  });
+});
